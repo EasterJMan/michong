6ffbba7a1123a689dbfc149d48968f0aadb235a0
EasterJMan <2568370392@qq.com>
2020-09-21 19:37:44 +0800

    米虫电影

---
+++ src/components/Home.vue
@@ -0,0 +1,101 @@
+<template>
+  <el-container class="home-container">
+    <!-- 页面头部区域 -->
+    <el-header>
+      <div>
+        <img
+          src="../assets/heima.png"
+          alt=""
+        >
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本  -->
+              <span>{{item.authName}}}</span>
+            </template>
+            <!-- 二级菜单 -->
+             <el-menu-item index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+               <!-- 图标 -->
+               <i class="el-icon-location"></i>
+               <!-- 文本  -->
+               <span>{{subItem.authName}}</span>
+              </template>
+             </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menuList: []
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(res)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img
+          src="../assets/logo.png"
+          alt=""
+        >
+      </div>
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button
+            type="primary"
+            @click="login('loginFormRef')"
+          >登录</el-button>
+          <el-button
+            type="info"
+            @click="resetLoginForm('loginFormRef')"
+          >重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单验证对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm (formName) {
+      // console.log(this)
+      this.$refs[formName].resetFields()
+    },
+    login (formName) {
+      this.$refs[formName].validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1. 将登录成功之后之后的token保存到客户端的sessionstorage中
+        // 1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        // 1.2 token只应在当前网站打开期间生效,所以将token保存在sessionstorage中
+        console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2.通过编程式导航跳转到后台主页,路由地址是/home
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  position: absolute;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 3px;
+    box-shadow: 0 0 5px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
